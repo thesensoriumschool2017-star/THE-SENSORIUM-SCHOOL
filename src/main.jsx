36a7089c3e8d440d2e121907918b1623a317a181
Your New Name <yournewemail@example.com>
2026-05-02 16:46:26 +0530

    Remove invite token redirect that interrupts Netlify Identity flow

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-if (
-  typeof window !== "undefined" &&
-  (window.location.hash.startsWith("#invite_token=") ||
-    window.location.hash.startsWith("#recovery_token=")) &&
-  !window.location.pathname.startsWith("/admin")
-) {
-  window.location.replace(`/admin/${window.location.hash}`);
-}
-
 if (typeof window !== "undefined" && window.netlifyIdentity) {
   window.netlifyIdentity.on("login", () => {
     window.location.href = "/admin/";
